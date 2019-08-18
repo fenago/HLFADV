6d68d2be987edb2e17ef5b3fcd14f5e4fb4cbc26
fenago <socrates73@gmail.com>
2019-08-18 11:33:47 -0400

    Update update_etc_hosts.sh

--- setup/update_etc_hosts.sh
+++ setup/update_etc_hosts.sh
@@ -2,17 +2,23 @@
 source    ./manage_hosts.sh
 
 HOSTNAME=peer1.acme.com
-removehost $HOSTNAME            &> /dev/null
-addhost $HOSTNAME
+#removehost $HOSTNAME            &> /dev/null
+# addhost $HOSTNAME
+HOSTNAME=peer1.acme.com
+sudo echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 HOSTNAME=peer1.budget.com
-removehost $HOSTNAME            &> /dev/null
-addhost $HOSTNAME
+#removehost $HOSTNAME            &> /dev/null
+#addhost $HOSTNAME
+sudo echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 HOSTNAME=orderer.acme.com
-removehost $HOSTNAME            &> /dev/null
-addhost $HOSTNAME
+#removehost $HOSTNAME            &> /dev/null
+#addhost $HOSTNAME
+sudo echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 HOSTNAME=postgresql
-removehost $HOSTNAME            &> /dev/null
-addhost $HOSTNAME
+#removehost $HOSTNAME            &> /dev/null
+#addhost $HOSTNAME
+sudo echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 HOSTNAME=explorer
-removehost $HOSTNAME            &> /dev/null
-addhost $HOSTNAME
+#removehost $HOSTNAME            &> /dev/null
+#addhost $HOSTNAME
+sudo echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
