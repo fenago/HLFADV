7c9867d2baa95597ec77e7f741a5c00418b07dbf
fenago <socrates73@gmail.com>
2019-08-18 12:07:07 -0400

    Update caserver-setup.sh

--- setup/caserver-setup.sh
+++ setup/caserver-setup.sh
@@ -1,7 +1,7 @@
 export PATH=$PATH:$GOROOT/bin
 
 # Sets up the fabric-ca-server & fabric-ca-client
-sudo apt install -y libtool libltdl-dev
+apt install -y libtool libltdl-dev
 
 # Document process leads to errors as it leads to pulling of master branch
 go get -u github.com/hyperledger/fabric-ca/cmd/...
@@ -11,13 +11,13 @@ go get -u github.com/hyperledger/fabric-ca/cmd/...
 # mv fabric-ca  $GOPATH/src/github.com/hyperledger
 # go install github.com/hyperledger/fabric-ca/cmd/...
 
-sudo cp $GOPATH/bin/*    /usr/local/bin
+cp $GOPATH/bin/*    /usr/local/bin
 
 
 
-sudo cp $GOPATH/bin/*    $PWD/../bin
+cp $GOPATH/bin/*    $PWD/../bin
 
-sudo rm $GOPATH/bin/* 
+rm $GOPATH/bin/* 
 
 echo "Done."
 
