72859255feb7b7472f3497484cee264f501d2145
Ather Tahir <ather.tahir@bentley.com>
2019-08-20 14:43:47 +0500

    remove sudo for katacoda

--- setup/docker.sh
+++ setup/docker.sh
@@ -15,14 +15,14 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/"${release_name}" $(lsb_release -cs) stable"
 apt-get update
 #    apt-get install -y "docker-ce=${DOCKER_VERSION}.*"
 apt-get install -y "docker-ce"
 docker info
 # usermod -aG docker vagrant
 echo "======= Adding $USER to the docker group ======="
-usermod -aG docker $USER
+usermod -aG docker root
 
 # Installs docker-compose
 apt-get install -y docker-compose
@@ -31,4 +31,3 @@ echo "======= Done. PLEASE LOG OUT & LOG Back In ===="
 echo "Then validate by executing    'docker ps'"
 
 # Restart is needed after this
-sudo - $USER
